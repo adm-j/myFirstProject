2ec1475e8d75a749de12ec8a6e4663eade9120c3
Adam <ad-91@live.co.uk>
2022-02-28 18:46:26 +0000

    Created simple addition program which continuously runs

--- main.cpp
+++ main.cpp
@@ -10,21 +10,48 @@ public: //makes class accessible outside of the class
         return number1 + number2;
     }
 
-    int subtrackNumbers(int number1, int number2) {
+    static int subtractNumbers(int number1, int number2) {
         return number1 - number2;
     }
+
+    void console(int num1, int num2) {
+        cout << "Enter first number to add together:" << "\n";
+        cin >> num1;
+        cout << "Enter second number:" << "\n";
+        cin >> num2;
+        cout << addNumbers(num1, num2) << "\n";
+    };
+
     vector<int> myDynamicArray; //creates a dynamic array
-    int myNonedynamicArray[10]; //Creates an array with 10 allocated slots in memory
-    myNonedynamicArray[0] = 10;
+    int myNonedynamicArray[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //Creates an array with 10 allocated slots in memory
 };
 
 int main() {
     maths num; // initialises the maths class as num within the main function
-//    std::cout << "Hello, World!" << std::endl;
+    int num1 = 0;
+    int num2 = 0;
+    int app = 0;
+    string appState;
+
     cout << "hello world!" << "\n";
 
-    cout << num.addNumbers(1, 2) << "\n";
-    cout << num.subtrackNumbers(5, 3);
+        while (app != 1) {
+            cout << "Enter begin or exit to continue:" << "\n";
+            cin >> appState;
+            if (appState == "exit") {
+                cout << "Exiting app" << "\n";
+                app = 1;
+            } else if (appState == "begin") {
+                cout << "Enter first number to add together:" << "\n";
+                cin >> num1;
+                cout << "Enter second number:" << "\n";
+                cin >> num2;
+                cout << "Answer:";
+                cout << num.addNumbers(num1, num2) << "\n";
+            } else {
+                cerr << "Invalid value: Please enter begin or exit." << "\n";
+            }
+        }
 
     return 0; //main must always return 0 - as a status code for success of main function
 }
